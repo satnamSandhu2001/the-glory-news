c76f32cf5a3dc75a4a2d00c4f9337f95b64f248f
Satnam Sandhu <89329812+satnamSandhu2001@users.noreply.github.com>
2021-12-16 09:46:24 +0530

    navbar active class

--- src/myComponents/Navbar.js
+++ src/myComponents/Navbar.js
@@ -1,76 +1,77 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../App.css";
 
 const Navbar = () => {
-    let date = new Date().toLocaleDateString();
-    return (
-        <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
-            <div className="container-fluid">
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarTogglerDemo03"
-                    aria-controls="navbarTogglerDemo03"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <h2 className="navbar-brand">The News Glory</h2>
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link to="/" className="nav-link" aria-current="page">
-                                Home
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/about" className="nav-link">
-                                About
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/business" className="nav-link">
-                                Business
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/entertainment" className="nav-link">
-                                Entertainment
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/general" className="nav-link">
-                                General
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/health" className="nav-link">
-                                Health
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/science" className="nav-link">
-                                Science
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/sports" className="nav-link">
-                                Sports
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/technology" className="nav-link">
-                                Technology
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-                <div className="Right-nav">{date}</div>
-            </div>
-        </nav>
-    );
+  let date = new Date().toLocaleDateString();
+  return (
+    <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo03"
+          aria-controls="navbarTogglerDemo03"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <h2 className="navbar-brand">The News Glory</h2>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink to="/" className="nav-link" aria-current="page">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/about" className="nav-link">
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/business" className="nav-link">
+                Business
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/entertainment" className="nav-link">
+                Entertainment
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/general" className="nav-link">
+                General
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/health" className="nav-link">
+                Health
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/science" className="nav-link">
+                Science
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/sports" className="nav-link">
+                Sports
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/technology" className="nav-link">
+                Technology
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="Right-nav">{date}</div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/myComponents/NewsComponent.js
+++ src/myComponents/NewsComponent.js
@@ -2,35 +2,42 @@ import React from "react";
 import "../App.css";
 
 const NewsComponent = (props) => {
-    let { title, description, urlToImage, url, publishedAt, source } = props;
-    return (
-        <div className="my-3 ">
-            <div className="card">
-                <img
-                    src={
-                        !urlToImage
-                            ? "https://scontent.fluh2-1.fna.fbcdn.net/v/t39.30808-1/p200x200/244239540_199292378975124_7642261756158151429_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=c6021c&_nc_ohc=t7ZRmAFkbuQAX-K0b01&_nc_ht=scontent.fluh2-1.fna&oh=86e9e6fd3f878b82bb01dc4870a3047d&oe=61B507E6"
-                            : urlToImage
-                    }
-                    className="card-img-top"
-                    alt="loading image"
-                />
-                <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
-                    <p className="card-text" style={!description ? { opacity: "0.4", fontStyle: "italic" } : { opacity: "1" }}>
-                        {!description ? "description not available" : description}
-                    </p>
-                    <a href={url} target="blank" className="btn btn-sm btn-primary">
-                        Read More
-                    </a>
-                </div>
-                <div className="card-footer">
-                    <small className="text-muted">Published by {source}</small>
-                    <br />
-                    <small className="text-muted">{publishedAt}</small>
-                </div>
-            </div>
+  let { title, description, urlToImage, url, publishedAt, source } = props;
+  return (
+    <div className="my-3 ">
+      <div className="card">
+        <img
+          src={
+            !urlToImage
+              ? "https://media.istockphoto.com/photos/abstract-digital-news-concept-picture-id1290904409?b=1&k=20&m=1290904409&s=170667a&w=0&h=6khncht98kwYG-l7bdeWfBNs_GGcG1pDqzLb6ZXhh7I="
+              : urlToImage
+          }
+          className="card-img-top"
+          alt="loading image"
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p
+            className="card-text"
+            style={
+              !description
+                ? { opacity: "0.4", fontStyle: "italic" }
+                : { opacity: "1" }
+            }
+          >
+            {!description ? "description not available" : description}
+          </p>
+          <a href={url} target="blank" className="btn btn-sm btn-primary">
+            Read More
+          </a>
         </div>
-    );
+        <div className="card-footer">
+          <small className="text-muted">Published by {source}</small>
+          <br />
+          <small className="text-muted">{publishedAt}</small>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default NewsComponent;
